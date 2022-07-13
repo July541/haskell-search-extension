6064ee91094e1372de03ca39b0db2d9bb41c533e
Lei Zhu <julytreee@gmail.com>
2022-07-13 18:04:39 +0800

    Core: Add Compat

--- extension/core/compat.ts
+++ extension/core/compat.ts
@@ -1,53 +1,73 @@
-function Compat() {
-    // Firefox doesn't support tags in search suggestion.
-    this.tagged = this.browserType() !== "firefox" ?
-        (tag, str) => `<${tag}>${str}</${tag}>` :
-        (_, str) => str;
-    this.match = (str) => this.tagged("match", str);
-    this.dim = (str) => this.tagged("dim", str);
-}
+class Compat {
+    public static browserType(): "firefox" | "edge" | "chrome" | "unknown" {
+        let userAgent = navigator.userAgent.toLowerCase()
 
-Compat.prototype.browserType = function () {
-    let userAgent = navigator.userAgent.toLowerCase();
-    if (userAgent.indexOf("firefox") !== -1) return "firefox";
-    if (userAgent.indexOf("edg") !== -1) return "edge";
-    if (userAgent.indexOf("chrome") !== -1) return "chrome";
-    return "unknown";
-};
+        if (userAgent.indexOf("firefox") !== -1)
+            return "firefox"
+        if (userAgent.indexOf("edg") !== -1)
+            return "edge"
+        if (userAgent.indexOf("chrome") !== -1)
+            return "chrome"
 
-Compat.prototype.omniboxPageSize = function () {
-    return {"firefox": 6, "edge": 7, "chrome": 8, "unknown": 6}[this.browserType()];
-};
-
-// Escape the five predefined entities to display them as text.
-Compat.prototype.escape = function (str) {
-    str = str || "";
-    return this.browserType() !== "firefox" ? str
-            .replace(/&/g, "&amp;")
-            .replace(/</g, "&lt;")
-            .replace(/>/g, "&gt;")
-            .replace(/"/g, "&quot;")
-            .replace(/'/g, "&#039;")
-        : str;
-};
-
-Compat.prototype.normalizeDate = function (date) {
-    let month = '' + (date.getMonth() + 1),
-        day = '' + date.getDate(),
-        year = date.getFullYear();
-    return [year, month.padStart(2, "0"), day.padStart(2, "0")].join('-');
-};
-
-Compat.prototype.capitalize = function (value) {
-    if (value) {
-        return value.charAt(0).toUpperCase() + value.slice(1);
+        return "unknown"
     }
-    return "";
-}
 
-Compat.prototype.eliminateTags = function (value) {
-    if (value) {
-        return value.replace(/<\/?(match|dim|code|em|strong)>/g, "");
+    public static omniboxPageSize(): number {
+        return { "firefox": 6,
+                 "edge": 7,
+                 "chrome": 8,
+                 "unknown": 6
+               } [this.browserType()]
     }
-    return "";
+    
+    /**
+     * Escape the five predefined entities to display them as text.
+     * @param str
+     */
+    public static escape(str?: string): string {
+        str = str || ""
+        return this.browserType() === "firefox"
+             ? str
+             : str.replace(/&/g, "&amp;")
+                  .replace(/</g, "&lt;")
+                  .replace(/>/g, "&gt;")
+                  .replace(/"/g, "&quot;")
+                  .replace(/'/g, "&#039;")
+    }
+
+    public static normalizedDateString(date: Date): string {
+        let month = (date.getMonth() + 1).toString() // FIXME: why plus one here?
+        let day = date.getDate().toString()
+        let year = date.getFullYear().toString()
+        return [year, month.padStart(2, "0"), day.padStart(2, "0")].join("-")
+    }
+
+    public static capitalize(s?: string): string {
+        if (!s || s.length === 0) {
+            return ""
+        }
+        return s.charAt(0).toUpperCase() + s.slice(1)
+    }
+
+    public static eliminateTags(value?: string): string {
+        if (!value) {
+            return ""
+        }
+        return value.replace(/<\/?(match|dim|code|em|strong)>/g, "")
+    }
+
+    public static tagged(tag: string, s: string): string {
+        if (this.browserType() === "firefox") {
+            return `<${tag}>${s}</${tag}>`
+        }
+        return s
+    }
+
+    public static taggedMatch(s: string): string {
+        return this.tagged("match", s)
+    }
+
+    public static taggedDim(s: string): string {
+        return this.tagged("match", s)
+    }
 }
