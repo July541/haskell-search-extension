baa02bfeced85caea9061f500677bd3280a83c69
Lei Zhu <julytreee@gmail.com>
2024-03-30 23:29:14 +0800

    Cabal package parser

--- hse-data-generator/exe/Hackage/Generator.hs
+++ hse-data-generator/exe/Hackage/Generator.hs
@@ -1,21 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Hackage.Generator where
 
 import Conduit qualified as C
 import Control.Monad (when)
 import Data.ByteString qualified as BS
 import Data.ByteString.UTF8 (toString)
+import Data.Char
 import Data.Conduit.List qualified as C
 import Data.Conduit.Tar qualified as CT
 import Data.Conduit.Zlib
+import Data.List.Extra (replace)
+import Data.Map qualified as Map
 import Data.Text qualified as T
 import Network.Connection (TLSSettings (TLSSettingsSimple))
 import Network.HTTP.Conduit qualified as HC
 import System.Directory
 import System.Time.Extra (duration, showDuration)
 
-data CabalPackage = CabalPackage
-  { packageName :: !T.Text
-  , packageSynopsis :: !T.Text
+newtype CabalPackage = CabalPackage
+  { packageSynopsis :: T.Text
   }
   deriving (Eq, Show)
 
@@ -43,6 +48,30 @@ test = do
 
 parseCabalTarball :: FilePath -> IO CabalPackage
 parseCabalTarball = undefined
+  where
+    f = undefined
+
+readCabal :: T.Text -> CabalPackage
+readCabal src = CabalPackage{..}
+  where
+    mp = Map.fromList $ lexCabal src
+    ask x = Map.findWithDefault [] x mp
+
+    packageSynopsis = T.unwords $ T.words $ T.unwords $ ask "synopsis"
+
+-- From hoogle
+lexCabal :: T.Text -> [(T.Text, [T.Text])]
+lexCabal = f . T.lines
+  where
+    f (x : xs)
+      | (white, x) <- T.span isSpace x
+      , (name, x) <- T.span (\c -> isAlpha c || c == '-') x
+      , not (T.null name)
+      , Just (':', x) <- T.uncons $ T.strip x
+      , (xs1, xs2) <- span (\s -> T.length (T.takeWhile isSpace s) > T.length white) xs =
+          (T.toLower name, T.strip x : replace ["."] [""] (map (T.strip . fst . T.breakOn "--") xs1)) : f xs2
+    f (_ : xs) = f xs
+    f [] = []
 
 -- | Download the Hackage index tarball if it doesn't exist with timing information
 downloadHackageIndexIfNecessaryWithTiming
