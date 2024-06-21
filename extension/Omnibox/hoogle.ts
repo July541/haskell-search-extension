8ec8473c10fbbfc5413d5b0e24b625bd7cb0e4f5
Lei Zhu <julytreee@gmail.com>
2024-06-21 23:47:46 +0800

    Support search on hoogle and change package search logic

--- extension/Omnibox/hoogle.ts
+++ extension/Omnibox/hoogle.ts
@@ -1,10 +1,15 @@
 export class Hoogle {
-    public static HOOGLE_PREFIX = "hoogle:";
+
+    static HOOGLE_BASE_URL: string = "https://hoogle.haskell.org/?hoogle=";
 
     public static hoogleSearch(query: string): chrome.omnibox.SuggestResult {
         return {
-            content: Hoogle.HOOGLE_PREFIX + query,
+            content: `${Hoogle.HOOGLE_BASE_URL}${query}`,
             description: `Search ${query} on [hoogle.haskell.org]`
         };
+    }
+
+    public static isHoogleUrl(url: string): boolean {
+        return url.startsWith(Hoogle.HOOGLE_BASE_URL);
     }
 }
--- extension/Omnibox/index.ts
+++ extension/Omnibox/index.ts
@@ -21,6 +21,16 @@ export class Omnibox {
     // Unused, I'm not sure it's time to optimize
     // cacheResults: CacheResults = new Map();
 
+    defaultContent: string = "";
+
+    /**
+     * Store the current input for `OnInputChanged` event,
+     * and when `OnInputEntered` event is fired, it will be used to determine whether to use the default content.
+     *
+     * We can use this rely on the fact that through up and down arrow keys, the `OnInputChanged` will not be fired.
+     */
+    currentInput: string = "";
+
     /**
      * 0 based
      */
@@ -30,40 +40,69 @@ export class Omnibox {
 
     bootstrap() {
         chrome.omnibox.onInputChanged.addListener((input: string, suggest) => {
-            const page = this.parsePage(input);
-            const startCount = page * PAGE_SIZE;
-            const endCount = startCount + PAGE_SIZE
+            this.currentInput = input;
 
-            const res: HackageData[] = fuzzysort.go(input, searchTargets, { key: "name" })
-                .map(x => new HackageData(x.target, x.obj.description))
-                .slice(startCount, endCount);
-
-            let suggestions: chrome.omnibox.SuggestResult[] = res
-                .map((x: HackageData) => ({
-                    content: x.name,
-                    description: x.description.length == 0 ? `[package] ${Compat.escape(x.name)}` :
-                        `[package] ${Compat.escape(x.name)} - ${Compat.escape(x.description)}`
-                }));
-            // insert hoogle search on the fiest position
-            suggestions.unshift(Hoogle.hoogleSearch(input));
-
-            if (suggestions.length > 0) {
-                const defaultSuggestion = suggestions.shift();
-                if (defaultSuggestion) {
-                    chrome.omnibox.setDefaultSuggestion({ description: defaultSuggestion.description });
-                }
-            }
-
+            const suggestions = this.searchPackage(input);
             suggest(suggestions);
         });
 
         chrome.omnibox.onInputEntered.addListener((input: string) => {
-            let url = `https://hackage.haskell.org/package/${input}`;
-            if (input.startsWith(Hoogle.HOOGLE_PREFIX)) {
-                const query = input.substring(Hoogle.HOOGLE_PREFIX.length);
-                url = `https://hoogle.haskell.org/?hoogle=${query}`;
+            if (input === this.currentInput) {
+                // If the input is the same as the this.currentInput,
+                // that means the user wants to use the first search result.
+                // So we need to use the default content as the search target(like package name)
+                input = this.defaultContent;
             }
+
+            if (Hoogle.isHoogleUrl(input)) {
+                chrome.tabs.update({ url: input });
+                return;
+            }
+
+            let url = `https://hackage.haskell.org/package/${input}`;
             chrome.tabs.update({ url });
         });
+    }
+
+    searchPackage(input: string): chrome.omnibox.SuggestResult[] {
+        const page = this.parsePage(input);
+        const startCount = page * PAGE_SIZE;
+        const endCount = startCount + PAGE_SIZE;
+        const res: HackageData[] = fuzzysort
+            .go(input, searchTargets, { key: "name" })
+            .map(x => new HackageData(x.target, x.obj.description))
+            .slice(startCount, endCount);
+
+        const suggestions: chrome.omnibox.SuggestResult[] = res
+            .map((x: HackageData) => ({
+                content: x.name,
+                description: x.description.length == 0
+                    ? `[package] ${Compat.escape(x.name)}`
+                    : `[package] ${Compat.escape(x.name)} - ${Compat.escape(x.description)}`
+            }));
+
+        this.coreceWithHoogle(suggestions, input);
+        this.adjustSuggestions(suggestions);
+
+        return suggestions;
+    }
+
+    coreceWithHoogle(suggestions: chrome.omnibox.SuggestResult[], input: string) {
+        const head = suggestions.shift();
+        suggestions.unshift(Hoogle.hoogleSearch(input));
+
+        if (head) {
+            suggestions.unshift(head);
+        }
+    }
+
+    adjustSuggestions(suggestions: chrome.omnibox.SuggestResult[]) {
+        const head = suggestions.shift();
+        if (head) {
+            // Save the content of the first suggestion, so that we can recover it
+            // if the user select the first suggestion while entering.
+            this.defaultContent = head.content;
+            chrome.omnibox.setDefaultSuggestion({ description: head.description });
+        }
     }
 }
