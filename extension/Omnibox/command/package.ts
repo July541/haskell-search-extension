4bf61e1783774633a10ed0fdade3b8ebbabc305b
Lei Zhu <julytreee@gmail.com>
2024-06-23 17:38:02 +0800

    Adjust struct

--- extension/Omnibox/command/package.ts
+++ extension/Omnibox/command/package.ts
@@ -1,6 +1,6 @@
 import fuzzysort from "fuzzysort";
 import { CommandHandler, SearchCache } from "./type";
-import { HackageData, hackageData } from "../data/hackageData";
+import { HackageData, hackageData } from "../data/hackage/hackageData";
 import { Compat } from "../Compat";
 import HoogleHandler from "./hoogle";
 
